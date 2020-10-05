788800a99f2f1573a7c6baef0a0bffc710d1a307
Joanna Galdzinska <joanna.galdzinska@gmail.com>
2020-10-05 18:40:54 +0300

    task 5 ready

---
+++ task-5.js
@@ -0,0 +1,45 @@
+function getAllPropValues(array, prop) {
+
+    const allPropValues = []
+
+    for ( const obj of array){
+        
+       // console.log(product)
+        for (const key in obj) {
+            if (key == prop){
+                allPropValues.push(obj[prop]);
+
+            }
+        }
+    }
+
+return allPropValues;
+    }
+  
+// Объекты и ожидаемый результат
+const products = [
+  { name: "Радар", price: 1300, quantity: 4 },
+  { name: "Радар", price: 1280, quantity: 2 },
+  { name: "Радар", price: 1320, quantity: 1 },
+  { name: "Сканер", price: 2700, quantity: 1 },
+  { name: "Сканер", price: 2500, quantity: 3 },
+  { name: "Дроид", price: 400, quantity: 7 },
+  { name: "Захват", price: 1200, quantity: 2 },
+];
+
+
+
+
+//console.log(products[0].name)
+
+
+console.log(getAllPropValues(products, 'name'));
+// ['Радар', 'Радар', 'Радар', 'Сканер', 'Сканер', 'Дроид', 'Захват']
+
+console.log(getAllPropValues(products, 'quantity'));
+// [4, 2, 1, 1, 3, 7, 2]
+
+console.log(getAllPropValues(products, 'category'));
+//  []
+
+
